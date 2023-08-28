d15a8eddad9e08547e4203b59dda6a98d716399a
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-08-28 13:22:06 -0400

    Allow Hugo server with 'production' processing
    This lets us look at the output of the resources pipes like minification
    and fingerprinting
    
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- tests/scripts/hugo-dev-serve.sh
+++ tests/scripts/hugo-dev-serve.sh
@@ -8,4 +8,4 @@ export HUGO_MODULE_REPLACEMENTS="github.com/wildtechgarden/wild-theme-shell-mod-
 export HUGO_RESOURCEDIR="$(pwd)/resources"
 export SITEROOT="$(pwd)"
 export SITECONFIG="$(pwd)"/tests/config/hugo.toml
-cd tests/config && hugo serve --buildDrafts --buildFuture --source "${SITEROOT}" --environment "development" --config "${SITECONFIG}"
+cd tests/config && hugo serve --buildDrafts --buildFuture --source "${SITEROOT}" --environment "${HUGO_ENV:-development}" --config "${SITECONFIG}"
