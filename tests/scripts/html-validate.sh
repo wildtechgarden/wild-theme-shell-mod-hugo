fc50bb14b9a09be0065b227386abecd022aa2f8f
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-09-04 17:52:21 -0400

    Add CSS validation by validator back to script
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- tests/scripts/html-validate.sh
+++ tests/scripts/html-validate.sh
@@ -6,13 +6,13 @@ set -o pipefail
 SITEROOT="$(pwd)"
 
 if java -jar ~/jar/vnu.jar --Werror $(find ${SITEROOT}/public -name '*.html') | tee html-validate.log; then
-	# if java -jar ~/jar/vnu.jar --Werror --css $(find ${SITEROOT}/public -name '*.css') | tee html-validate.log; then
-	echo "ok"
-	exit 0
-	# else
-	# 	echo "not ok"
-	# 	exit 1
-	# fi
+	if java -jar ~/jar/vnu.jar --Werror --css $(find ${SITEROOT}/public -name '*.css') | tee -a html-validate.log; then
+		echo "ok"
+		exit 0
+	else
+		echo "not ok"
+		exit 1
+	fi
 else
 	echo "not ok"
 	exit 1
