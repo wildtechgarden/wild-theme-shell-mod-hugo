ea3b991d58af478c0a4e9f1ededb03bb26a19f59
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-11-07 13:12:18 -0500

    Experiment with web components; not currently deployable (WIP)
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- scripts/local_build_deploy.sh
+++ scripts/local_build_deploy.sh
@@ -9,7 +9,7 @@ rm -rf public
 export BASEURL="https://www.wild-theme-shell.wtg-demos.ca/"
 export HUGO_RESOURCEDIR="$(pwd)"/resources
 # export HUGO_MODULE_REPLACEMENTS="github.com/wildtechgarden/wild-theme-shell-mod-hugo -> $(pwd)"
-export SITEROOT="$(pwd)"
+export SITEROOT="$(pwd)/tests/config"
 export SITECONFIG="$(pwd)"/tests/config/test-config.toml
 export TARGET="$(pwd)"/public
 export CURDIR="$(pwd)"
