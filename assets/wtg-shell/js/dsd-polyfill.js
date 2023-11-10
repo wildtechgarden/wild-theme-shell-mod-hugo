5b71db3043a2d27f0b40d9e11b5068c1bc60d264
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-11-10 01:56:32 -0500

    Fix use of polyfill and improve JS imports
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- assets/wtg-shell/js/dsd-polyfill.js
+++ assets/wtg-shell/js/dsd-polyfill.js
@@ -1,9 +1,9 @@
-window.onload = ";(function attachShadowRoots(root) { \
-	root.querySelectorAll(\"template[shadowrootmode]\").forEach(function(template) { \
-		var mode = template.getAttribute(\"shadowrootmode\"); \
-		var shadowRoot = template.parentNode.attachShadow(mode); \
-		shadowRoot.appendChild(template.content); \
-		template.remove(); \
-		attachShadowRoots(shadowRoot); \
-	}); \
-})(document);"
+function attachShadowRoots(root) { /* eslint-disable-line no-unused-vars */
+	root.querySelectorAll("template[shadowrootmode]").forEach(function(template) {
+		var mode = template.getAttribute("shadowrootmode");
+		var shadowRoot = template.parentNode.attachShadow({ "mode": mode});
+		shadowRoot.appendChild(template.content);
+		template.remove();
+		attachShadowRoots(shadowRoot);
+	});
+}
