8b3408c2d13dcb0c82b5b43c45821fec87f03499
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-11-09 23:35:27 -0500

    Disable HTML/CSS validation due to DSD newness
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- tests/scripts/html-validate.sh
+++ tests/scripts/html-validate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+# cspell:ignore filterpattern
 set -e
 set -o pipefail
 
 SITEROOT="$(pwd)"
 
-if java -jar ~/jar/vnu.jar --Werror $(find ${SITEROOT}/public -name '*.html') | tee html-validate.log; then
-	if java -jar ~/jar/vnu.jar --Werror --css $(find ${SITEROOT}/public -name '*.css') | tee -a html-validate.log; then
+if java -jar ~/jar/vnu.jar --Werror --filterpattern "(Attribute .shadowrootmode. not allowed on element .template. at this point\.)" $(find ${SITEROOT}/public -name '*.html') | tee html-validate.log; then
+	if java -jar ~/jar/vnu.jar --Werror --filterpattern "(.*\: Property.*(block|inline|start-start|start-end|end-start|end-end).*doesn't exist\.|.*Unknown pseudo-element or pseudo-class .:host.|CSS\: Parse Error\.)" --css $(find ${SITEROOT}/public -name '*.css') | tee -a html-validate.log; then
 		echo "ok"
 		exit 0
 	else
